3bc2bf5af33a87505bd6b768075a92a23f01a8ee
yuricca <yuricca531@gmail.com>
2021-08-09 17:08:23 +0900

    バリデーションの追加
    ○コードの簡素化
    ○不要な記述の削除
    ○以下の事項の追加
    　①「パスワード」は半角英数字混合での入力が必須であること
    　②「お名前(全角)」は全角での入力が必須であること
    　③「お名前カナ(全角) 」は全角カタカナでの入力が必須であること

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: ZENKAKU_REGEX, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: { with: ZENKAKU_REGEX, message: "is invalid. Input full-width characters."}
+    validates :last_name_kana, format: { with: KANA_REGEX, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_kana, format: { with: KANA_REGEX, message: "is invalid. Input full-width katakana characters."}
+    validates :birthday
+  end
 end
