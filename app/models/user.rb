d17c23905aba8855d0a913d3eaab7d33a24a007d
yuricca <yuricca531@gmail.com>
2021-08-09 17:38:05 +0900

    エラーメッセージの修正
    パスワードだけ日本語だったので

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
   KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
 
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'should be contain both letters and numbers.'
 
   with_options presence: true do
     validates :nickname
