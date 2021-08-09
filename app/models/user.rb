917e277f48178723e11f577c686c9a9114a940a1
yuricca <yuricca531@gmail.com>
2021-08-09 13:02:10 +0900

    バリデーション、エラーメッセージに関する修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
