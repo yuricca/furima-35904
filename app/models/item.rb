a81773197d2b9f4a5ef39ea37bc576bd8df7ec88
yuricca <yuricca531@gmail.com>
2021-08-15 07:50:52 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :delivery_day
 end
