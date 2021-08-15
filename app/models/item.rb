c11e8eeb5c24cee5bb54220b1209fed39b04af7a
yuricca <yuricca531@gmail.com>
2021-08-15 22:03:09 +0900

    バリデーションの設定
    ActiveHashの軽微な修正を含む

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,25 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :delivery_fee
   belongs_to :prefecture
   belongs_to :delivery_day
+
+  with_options presence: true do
+    validates :name
+    validates :detail
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 , only_integer: true }
+  end
+
+  with_options numericality: { other_than: 1 , message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :delivery_day_id
+  end
 end
