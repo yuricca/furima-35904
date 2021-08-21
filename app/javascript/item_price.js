e4af897b9583f51773900b98e1e8c59e59119af0
yuricca <yuricca531@gmail.com>
2021-08-21 22:43:40 +0900

    販売手数料と販売利益の計算結果表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,11 @@
+function input (){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+    const  addProfitDom = document.getElementById("profit");
+      addProfitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
+})
+};
+window.addEventListener('load', input);
