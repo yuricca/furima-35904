e2e28066d6240cdd4010df5fd7cafd84776eae08
yuricca <yuricca531@gmail.com>
2021-08-22 13:21:24 +0900

    コードレビューを受けての修正
    ①authenticate_user! の記述場所をapplication_controller.rb → items_controller.rbに修正
    ②createメソッドの修正
    ③テストの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
   end
 
@@ -7,7 +9,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
