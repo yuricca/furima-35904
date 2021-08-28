d7f2b9dcf592c5cbd4bd0d502a1dccf4419feb67
yuricca <yuricca531@gmail.com>
2021-08-28 22:46:29 +0900

    編集画面の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,7 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
   end
 
   private
@@ -27,5 +30,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :detail, :category_id, :status_id, :delivery_fee_id, :prefecture_id,
                                  :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
