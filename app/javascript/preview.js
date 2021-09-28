786f9751dd5a55a5da7f66765363983c9d09ea90
yuricca <yuricca531@gmail.com>
2021-09-28 21:40:35 +0900

    プレビュー機能の実装
    card.jsとitem_price.jsのエラーの解消を含む

---
+++ app/javascript/preview.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+  if (document.getElementById('item-image')){
+    const ImageList = document.getElementById('image-list');
+    document.getElementById('item-image').addEventListener('change', function(e){
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    });
+  }
+});
